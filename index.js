34c0490baca56e717e643782c8191313968a7f5d
Joseph Autorino <staircasesoul@gmail.com>
2021-04-05 23:27:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrpendKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitt() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
 // Add your functions and code here
