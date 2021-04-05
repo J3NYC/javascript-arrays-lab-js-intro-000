78bbab35fb98527309a86b5ebe86aba991d6e473
Joseph Autorino <staircasesoul@gmail.com>
2021-04-05 23:37:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,4 +27,8 @@ function prependKitten(name) {
 function removeFirstKitten() {
   return kittens.slice(1)
 }
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length -1)
+}
 // Add your functions and code here
